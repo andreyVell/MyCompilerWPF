1cd301dd53d14ec4cebff1cc8b5964fd3e19110a
andreyVell <andreyperm109@mail.ru>
2021-11-15 09:55:58 +0500

    Lexer

---
+++ CToken.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCompilerWPF
+{
+    class CToken
+    {
+        enum ETokenType
+        {
+            ttOper,
+            ttIdent,
+            ttValue
+        }
+        enum EOperator
+        {
+
+        }
+        enum EValType
+        {
+            vtInt = 0x1,
+            vtReal = 0x2,
+            vtString = 0x4,
+            vtChar = 0x8
+        }
+    }
+}
