466bdf49e843cc8721a6df88d7b58ea809f3cd5e
andreyVell <andreyperm109@mail.ru>
2021-11-23 21:04:23 +0500

    add some visual staff and hotkey F5

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+//using System.Windows.Controls;
+//using System.Windows.Data;
+//using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+//using System.Windows.Media;
+//using System.Windows.Media.Imaging;
+//using System.Windows.Navigation;
+//using System.Windows.Shapes;
 
 namespace MyCompilerWPF
 {
@@ -33,14 +33,17 @@ namespace MyCompilerWPF
             textBoxOutput.Margin = new Thickness(10, e.NewSize.Height-marginHeightTextBox, 10, 5);
             labelResult.Margin = new Thickness(10, e.NewSize.Height - marginHeightTextBox - 25, 0, 0);
         }
-
         private void Run_Click(object sender, RoutedEventArgs e)
         {
             textBoxOutput.Text = string.Empty;
             PascalCompiler compiler = new PascalCompiler();
             compiler.Compilate(textBoxInput.Text);
-            textBoxOutput.Text = compiler.GetResult();            
+            textBoxOutput.Text = compiler.GetResult();
+        }
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F5)
+                Run_Click(sender, e);
         }
     }
-    
 }
